4769595e0702d6162ce0227d4f3da07da173095c
Zakaria Ennaqui <zakariaennaqui.pro@gmail.com>
2026-06-27 00:59:20 +0100

    .

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -2,19 +2,20 @@ import type { Project } from "../types";
 
 export const projects: Project[] = [
   {
-    title: "E-commerce App",
+    title: "...",
     description:
-      "Full-stack e-commerce solution with React, Node.js, and MongoDB. Features include user authentication, payment integration, and admin dashboard.",
-    image: "/images/projects/ecommerce-seamless-scaled.jpg",
-    tech: ["React Js", "Node Js"],
-    github: "https://github.com/zakariaennaqui/site-ecommerce-uml",
-    live: "https://ecommerce-app-6dsg.vercel.app",
+      "...",
+    image: "/images/projects/image.jpg",
+    tech: ["...", "..."],
+    github: "https://github.com/zakariaennaqui/...",
+    live: "https://...",
   },
   {
     title: "Other Projects ...",
     description:
       "Explore more of my projects on GitHub, showcasing my skills in web development, software engineering, and more.",
-    tech: ["     "],
+    image: "/images/projects/github.jpg",
+    tech: [" "],
     github: "https://github.com/zakariaennaqui",
   },
 ];
